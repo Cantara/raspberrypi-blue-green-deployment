53646221b897e1594d5688b21c7d817e4c7b35fd
baardl <bard.lind@gmail.com>
2020-10-11 12:40:52 +0200

    #3 Blue-Green is working as currently expected.

--- blue-green-deployment.sh
+++ blue-green-deployment.sh
@@ -4,6 +4,8 @@
 #./start-nodes.sh
 
 # Config
+primaryNode=$(./scripts/isPrimaryGreenOrBlue.sh)
+echo Primary: $primaryNode
 fallbackNode=$(./scripts/isFallbackGreenOrBlue.sh)
 echo Fallback: $fallbackNode
 NODE_DIR=none
@@ -24,7 +26,23 @@ else
 fi
 cp target/blueGreenService.jar $NODE_DIR
 # Kill the node
-sh ./scripts/kill-node.sh $NODE_DIR
+nohup sh ./scripts/kill-node.sh $NODE_DIR
 #Start a new node which will assume primary role.
-sh ./scripts/start-candidate.sh $NODE_DIR $NODE_PORT 5051 http://localhost:$PRIMARY_PORT/
+#echo "Start: ./scripts/start-candidate.sh $NODE_DIR $NODE_PORT http://localhost:$PRIMARY_PORT/blueGreenService/bluegreenstatus/requestPrimary"
+sh ./scripts/start-candidate.sh $NODE_DIR $NODE_PORT http://localhost:$PRIMARY_PORT/blueGreenService/bluegreenstatus/requestPrimary
+sleep 2
+echo
+echo Find current ProcessIds
 sh ./scripts/find-processIds.sh
+echo
+echo
+newNodeHealth=$(curl -s http://localhost:$NODE_PORT/blueGreenService/health)
+echo New Primary Health: $newNodeHealth
+sleep 1
+newFallbackHealth=$(curl -s http://localhost:$PRIMARY_PORT/blueGreenService/health)
+echo New Fallback Health: $newFallbackHealth
+echo
+echo
+primaryNode=$(./scripts/isPrimaryGreenOrBlue.sh)
+echo Updated Primary to: $primaryNode
+
